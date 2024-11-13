354642262bab06ec01d0b1e2f889f42e24137c5c
Alexandre GALLAIS <37043327+AlexandreGallais@users.noreply.github.com>
2024-11-13 22:52:47 +0100

    💥 Change to Angular

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,14 @@
-import type { Preview } from '@storybook/web-components';
-import '../src/styles/ffa.scss';
+import type { Preview } from "@storybook/angular";
 
 const preview: Preview = {
-    parameters: {
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i,
-            },
-        },
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
     },
+  },
 };
 
 export default preview;
